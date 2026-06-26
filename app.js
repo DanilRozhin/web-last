ac6438f8fb783703d264ef3651774d438aad12a8
Danil Rozhin <144659954+DanilRozhin@users.noreply.github.com>
2026-06-26 18:52:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,56 @@
 const express = require('express');
-const multer = require('multer');
+const busboy = require('busboy');
 const zlib = require('zlib');
 const app = express();
 
 const USER_LOGIN = 'danilrozhin';
 
-const upload = multer();
-
 app.get('/login', (req, res) => {
   res.send(USER_LOGIN);
 });
 
-app.post('/zipper', upload.single('file'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).send('No file uploaded');
-  }
+app.post('/zipper', (req, res) => {
+  const bb = busboy({ headers: req.headers });
+  let fileBuffer = Buffer.alloc(0);
+  let fileReceived = false;
 
-  const fileBuffer = req.file.buffer;
+  bb.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    fileReceived = true;
+    file.on('data', (chunk) => {
+      fileBuffer = Buffer.concat([fileBuffer, chunk]);
+    });
+  });
 
-  zlib.gzip(fileBuffer, (err, compressed) => {
-    if (err) {
-      return res.status(500).send('Compression error');
+  bb.on('field', (fieldname, value) => {
+    if (!fileReceived) {
+      fileBuffer = Buffer.from(value);
+      fileReceived = true;
+    }
+  });
+
+  bb.on('finish', () => {
+    if (!fileReceived || fileBuffer.length === 0) {
+      return res.status(400).send('No file uploaded');
     }
 
-    res.set({
-      'Content-Type': 'application/gzip',
-      'Content-Disposition': 'attachment; filename="result.gz"',
+    zlib.gzip(fileBuffer, (err, compressed) => {
+      if (err) {
+        return res.status(500).send('Compression error');
+      }
+
+      res.set({
+        'Content-Type': 'application/gzip',
+        'Content-Disposition': 'attachment; filename="result.gz"',
+      });
+      res.send(compressed);
     });
-    res.send(compressed);
   });
+
+  bb.on('error', () => {
+    res.status(500).send('Internal server error');
+  });
+
+  req.pipe(bb);
 });
 
-app.listen(3000);
+app.listen(5000, '0.0.0.0');
